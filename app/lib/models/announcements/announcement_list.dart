f3ba99fb66e7e8ef8d39271876072265cdd19596
byobelixasterix <deepakbhatia@users.noreply.github.com>
2023-07-09 20:46:48 +0100

    Update announcement_list.dart

--- app/lib/models/announcements/announcement_list.dart
+++ app/lib/models/announcements/announcement_list.dart
@@ -9,7 +9,7 @@ import '../../models/http_exception.dart';
 
 import 'announcement_class.dart';
 
-const _announcementApiUrl = 'https://server-3vgb3n.bunnyenv.com/api/announcement/';
+const _announcementApiUrl = String.fromEnvironment('API_BASE_URL') +'/api/announcement/';
 
 List<Announcement> parseAnnouncements(String response) {
   final el = json.decode(response) as List<dynamic>;
